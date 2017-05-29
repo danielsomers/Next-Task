770b01f04d4f7253e1a70ab1413459afa51e5f5c
Daniel Somers <mail@danielsomers.com>
2017-05-28 23:04:55 -0400

    Added id to model

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,35 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 
 namespace Models
 {
-    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
     public class Task
     {
-        private readonly RequestDelegate _next;
-
-        public Task(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public Task Invoke(HttpContext httpContext)
-        {
-
-            return _next(httpContext);
-        }
+        public Guid Id { get; set; }
     }
 
-    // Extension method used to add the middleware to the HTTP request pipeline.
-    public static class TaskExtensions
-    {
-        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<Task>();
-        }
-    }
 }
